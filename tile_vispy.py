a831329ab4fde775276127365ea138ef01a751bd
Aaron Fenyes <afenyes@math.toronto.edu>
2020-09-21 12:46:00 +0200

    Try out box filter
    This requires keeping track of pixel orientation.

--- tile_vispy.py
+++ tile_vispy.py
@@ -84,7 +84,7 @@ void main_multi() {
   gl_FragColor = vec4(color_sum / 9., 1.);
 }
 
-void main_area() {
+void main_tophat() {
   // find screen coordinate
   vec2 u = VIEW*(2.*gl_FragCoord.xy - resolution) / shortdim;
   float r_sq = dot(u, u);
@@ -115,7 +115,7 @@ void main_area() {
           if (onsides >= 3) {
             // we're in the fundamental domain, on the negative side of every mirror
             
-            //get the distance to the nearest mirror
+            // get the distance to the nearest mirror
             float mirror_dist = -SQRT2 * max(max(mirror_prod[0], mirror_prod[1]), mirror_prod[2]);
             /*if (mirror_dist > r_px) {
               gl_FragColor = vec4(0., 1., 0., 1.);
@@ -130,7 +130,85 @@ void main_area() {
             float coverage = 0.5 + 0.5*min(mirror_dist / r_px, 1.);
             
             float pos_color = mod(flips, 2);
-            float px_color = coverage*pos_color + (1.-coverage)*(1.-pos_color);
+            float px_color = mix(1.-pos_color, pos_color, coverage);
+            gl_FragColor = vec4(vec3(px_color), 1.);
+            return;
+          }
+        }
+      }
+    }
+  }
+  gl_FragColor = vec4(0.25, 0.15, 0.35, 1.);
+}
+
+void main_box() {
+  // find screen coordinate
+  vec2 u = VIEW*(2.*gl_FragCoord.xy - resolution) / shortdim;
+  float r_sq = dot(u, u);
+  
+  // find pixel bounds, for antialiasing
+  float r_px_screen = VIEW / shortdim; // the inner radius of a pixel in the Euclidean metric of the screen
+  float r_px = 2.*r_px_screen / (1.-r_sq); // the approximate inner radius of our pixel in the hyperbolic metric
+  vec2 dir_px = vec2(1., 0.); // the direction toward the east edge of our pixel, represented as a vector in the x, y plane
+  
+  // reduce to fundamental domain
+  float mirror_prod [3];
+  if (r_sq < 1.) {
+    vec3 v = vec3(2.*u, 1.+r_sq) / (1.-r_sq);
+    /*if (mprod(v, v) < -0.5) {
+      gl_FragColor = vec4(0.2, 0., 1., 1.);
+      return;
+    }*/
+    int flips = 0;
+    int onsides = 0; // how many times in a row we've been on the negative side of a mirror
+    while (flips < 40) {
+      for (int k = 0; k < 3; k++) {
+        mirror_prod[k] = mprod(v, mirrors[k]);
+        if (mirror_prod[k] > 0.) {
+          v -= 2.*mirror_prod[k]*mirrors[k];
+          dir_px -= 2.*dot(dir_px, mirrors[k].xy)*mirrors[k].xy;
+          flips += 1;
+          onsides = 0;
+        } else {
+          onsides += 1;
+          if (onsides >= 3) {
+            // we're in the fundamental domain, on the negative side of every mirror
+            
+            // find the nearest mirror
+            int near;
+            if (mirror_prod[0] >= mirror_prod[1] && mirror_prod[0] >= mirror_prod[2]) {
+              near = 0;
+            } else if (mirror_prod[1] >= mirror_prod[2]) {
+              near = 1;
+            } else {
+              near = 2;
+            }
+            
+            // find the distance to the nearest mirror, in pixel radii, and the
+            // the pixel direction in the frame where the mirror normal
+            // direction is (1, 0)
+            float rel_dist = -SQRT2 * mirror_prod[near] / r_px;
+            vec2 rel_dir = abs(vec2(
+              dir_px.x * mirrors[near].x + dir_px.y * mirrors[near].y,
+              -dir_px.x * mirrors[near].y + dir_px.y * mirrors[near].x
+            ));
+            if (rel_dir.y > rel_dir.x) rel_dir = rel_dir.yx;
+            rel_dir /= length(rel_dir);
+            
+            // estimate how much of our pixel is on the positive side of the nearest mirror
+            float overflow;
+            dir_px /= length(dir_px);
+            if (rel_dist >= rel_dir.x + rel_dir.y) {
+              overflow = 0.;
+            } else if (rel_dist >= rel_dir.x - rel_dir.y) {
+              float lap = rel_dir.x + rel_dir.y - rel_dist;
+              overflow = lap*lap / (8.*rel_dir.x*rel_dir.y);
+            } else {
+              overflow = 0.5*(1. - rel_dist/rel_dir.x);
+            }
+            
+            float pos_color = mod(flips, 2);
+            float px_color = mix(pos_color, 1.-pos_color, overflow);
             gl_FragColor = vec4(vec3(px_color), 1.);
             return;
           }
@@ -143,7 +221,8 @@ void main_area() {
 
 void main() {
   /*main_multi();*/
-  main_area();
+  /*main_tophat();*/
+  main_box();
 }
 ''')
 
