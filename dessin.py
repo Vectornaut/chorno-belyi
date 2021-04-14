9d3472ab48d8c248c20dadf44d4b6016388d2936
Aaron Fenyes <afenyes@math.toronto.edu>
2021-04-14 19:02:40 +0200

    Find a reasonable-looking spanning tree
    Works for two test dessins. Needs to be tested on more.

--- dessin.py
+++ dessin.py
@@ -40,54 +40,62 @@ class Dessin(Covering):
       
       # find a fundamental domain
       s = self.group.gens()
-      self.route = [[[]] + (self.degree-1)*[None], self.degree*[None]]
+      self.route = [self.degree*[None], self.degree*[None]]
       self.edge_gluings = []
       self.vertex_gluings = []
-      frontier = [[1], []]
-      side = 0
+      frontier = [[1], [1]]
+      side = 1
       color = 1
-      skips = 0
-      while skips < 2:
-        print('frontier ' + str(side) + ': ' + str(frontier[side]))
-        if frontier[side]:
-          skips = 0
-          print('side: ' + str(side))
-          edge = frontier[side].pop(0)
-          for cnt in reversed(range(self.orders[side])):
-            print('edge: ' + str(edge))
-            print(self.route[0])
-            print(self.route[1])
-            # explore the half-edge on the other side
-            if self.route[1-side][edge-1] == None:
-              self.route[1-side][edge-1] = self.route[side][edge-1]
-              frontier[1-side].append(edge)
+      while True:
+        if not frontier[side]:
+          side = 1-side
+        if not frontier[side]:
+          break
+        edge = frontier[side].pop(0)
+        print('side ' + str(side) + ', edge ' + str(edge))
+        print('|' + str(self.route[0]))
+        print('|' + str(self.route[1]))
+        # explore the opposite half-edge
+        if self.route[1-side][edge-1] == None:
+          # route to the opposite half-edge from this one
+          if self.route[side][edge-1] == None:
+            self.route[1-side][edge-1] = []
+          else:
+            self.route[1-side][edge-1] = self.route[side][edge-1]
+          print('flip')
+          print('|' + str(self.route[0]))
+          print('|' + str(self.route[1]))
+          
+          # petal the opposite half-edge
+          for cnt in range(self.orders[1-side]-1):
+            next_edge = s[1-side](edge)
+            print('  petaling; next edge: ' + str(next_edge))
+            if self.route[1-side][next_edge-1] == None:
+              self.route[1-side][next_edge-1] = self.route[1-side][edge-1] + [1-side]
+              frontier[1-side].append(next_edge)
             else:
-              self.edge_gluings.append((edge, color))
+              self.vertex_gluings.append((1-side, edge, next_edge, color))
+              print('  vertex gluing ' + str(self.vertex_gluings[-1]))
               color += 1
-            print('explore opposite half-edge')
-            print(self.route[0])
-            print(self.route[1])
-            
-            # rotate to the next half-edge
-            if cnt > 0:
-              next_edge = s[side](edge)
-              print('next edge: ' + str(next_edge))
-              if self.route[side][next_edge-1] == None:
-                self.route[side][next_edge-1] = self.route[side][edge-1] + [side]
-              else:
-                self.vertex_gluings.append((side, edge, next_edge, color))
-                print('vertex gluing ' + str((side, edge, next_edge, color)))
-                color += 1
-                break
-              edge = next_edge
-        else:
-          skips += 1
+              break
+            print('  |' + str(self.route[0]))
+            print('  |' + str(self.route[1]))
+            edge = next_edge
+        elif side == 0:
+          # glue to the opposite half-edge
+          self.edge_gluings.append((edge, color))
+          print('  edge gluing ' + str(self.edge_gluings[-1]))
+          color += 1
         side = 1-side
 
 ##[TEST]
 if __name__ == '__main__':
-  ##dessin = Dessin([(1,2,3,4),(1,3,4,2),(1,3,4)], 'a')
-  dessin = Dessin([(1,2,3,4,5),(1,2,5,4),(1,2,4,3)], 'a')
+  ##dessin = Dessin([(1,2,3,4),(1,3,4,2),(1,3,4)], 'a') # 4T5-4_4_3.1
+  dessin = Dessin([(1,2,3,4,5),(1,2,5,4),(1,2,4,3)], 'a') # 5T3-5_4.1_4.1
+  print('final routes')
   print(dessin.route[0])
   print(dessin.route[1])
+  print('final edge gluings')
+  print(dessin.edge_gluings)
+  print('final vertex gluings')
   print(dessin.vertex_gluings)
