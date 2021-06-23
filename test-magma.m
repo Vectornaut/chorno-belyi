fb5a988728e449ff1b259f7d000b7b731dc3739e
Sam Schiavone <sam.schiavone@gmail.com>
2021-06-23 09:59:41 -0400

    1-t, 1/t

--- test-magma.m
+++ test-magma.m
@@ -21,17 +21,19 @@ printf "hypergeometric parameters = %o, %o, %o\n", A-C+1, B-C+1, 2-C;
 HypergeometricSeries2F1(A-C+1, B-C+1, 2-C, test_z);
 
 print "values of series centered at 1";
+test_z_1 := 1 - test_z;
 /*
   h1_1 = hypergeometric([C-A, C-B], [1-A-B+C], t).series(t, prec) # y_6
   h2_1 = hypergeometric([  A,   B], [1+A+B-C], t).series(t, prec) # y_5
 */
 printf "hypergeometric parameters = %o, %o, %o\n", C-A, C-B, 1-A-B+C;
-HypergeometricSeries2F1(C-A, C-B, 1-A-B+C, test_z);
+HypergeometricSeries2F1(C-A, C-B, 1-A-B+C, test_z_1);
 printf "hypergeometric parameters = %o, %o, %o\n", A, B, 1+A+B-C;
-HypergeometricSeries2F1(A, B, 1+A+B-C, test_z);
+HypergeometricSeries2F1(A, B, 1+A+B-C, test_z_1);
 
 
 test_z := 1.5+0.1*I;
+test_z_1 := 1 - test_z;
 printf "test input = %o\n", test_z;
 print "values of series centered at 1";
 /*
@@ -39,16 +41,17 @@ print "values of series centered at 1";
   h2_1 = hypergeometric([  A,   B], [1+A+B-C], t).series(t, prec) # y_5
 */
 printf "hypergeometric parameters = %o, %o, %o\n", C-A, C-B, 1-A-B+C;
-HypergeometricSeries2F1(C-A, C-B, 1-A-B+C, test_z);
+HypergeometricSeries2F1(C-A, C-B, 1-A-B+C, test_z_1);
 printf "hypergeometric parameters = %o, %o, %o\n", A, B, 1+A+B-C;
-HypergeometricSeries2F1(A, B, 1+A+B-C, test_z);
+HypergeometricSeries2F1(A, B, 1+A+B-C, test_z_1);
 
 print "values of series centered at oo";
+test_z_oo := 1/test_z;
 /*
   h1_inf = hypergeometric([B, 1+B-C], [1+B-A], t).series(t, prec) # y_10
   h2_inf = hypergeometric([A, 1+A-C], [1+A-B], t).series(t, prec) # y_9
 */
 printf "hypergeometric parameters = %o, %o, %o\n", B, 1+B-C, 1+B-A;
-HypergeometricSeries2F1(B, 1+B-C, 1+B-A, test_z);
+HypergeometricSeries2F1(B, 1+B-C, 1+B-A, test_z_oo);
 printf "hypergeometric parameters = %o, %o, %o\n", A, 1+A-C, 1+A-B;
-HypergeometricSeries2F1(A, 1+A-C, 1+A-B, test_z);
+HypergeometricSeries2F1(A, 1+A-C, 1+A-B, test_z_oo);
--- test-sage.sage
+++ test-sage.sage
@@ -22,30 +22,34 @@ print("hypergeometric parameters = %s, %s, %s" % (A-C+1, B-C+1, 2-C))
 h2_0 = hypergeometric([A-C+1, B-C+1], [2-C], t).series(t, prec)
 print(h2_0(t=test_z))
 
+
+test_z_1 = 1 - test_z
 print("values of series centered at 1")
 print("hypergeometric parameters = %s, %s, %s" % (C-A, C-B, 1-A-B+C))
 h1_1 = hypergeometric([C-A, C-B], [1-A-B+C], t).series(t, prec) # y_6
-print(h1_1(t=test_z))
+print(h1_1(t=test_z_1))
 print("hypergeometric parameters = %s, %s, %s" % (A,B,1+A+B-C))
 h2_1 = hypergeometric([A,B], [1+A+B-C], t).series(t, prec) # y_5
-print(h2_1(t=test_z))
+print(h2_1(t=test_z_1))
 
 
 test_z = 1.5+0.1*I
+test_z_1 = 1 - test_z
 print("test input value = %s" % test_z)
 
 print("values of series centered at 1")
 print("hypergeometric parameters = %s, %s, %s" % (C-A, C-B, 1-A-B+C))
 h1_1 = hypergeometric([C-A, C-B], [1-A-B+C], t).series(t, prec) # y_6
-print(h1_1(t=test_z))
+print(h1_1(t=test_z_1))
 print("hypergeometric parameters = %s, %s, %s" % (A,B,1+A+B-C))
 h2_1 = hypergeometric([A,B], [1+A+B-C], t).series(t, prec) # y_5
-print(h2_1(t=test_z))
+print(h2_1(t=test_z_1))
 
+test_z_oo = 1/test_z
 print("values of series centered at oo")
 print("hypergeometric parameters = %s, %s, %s" % (B, 1+B-C, 1+B-A))
 h1_inf = hypergeometric([B, 1+B-C], [1+B-A], t).series(t, prec) # y_10
-print(h1_inf(t=test_z))
+print(h1_inf(t=test_z_oo))
 print("hypergeometric parameters = %s, %s, %s" % (A, 1+A-C, 1+A-B))
 h2_inf = hypergeometric([A, 1+A-C], [1+A-B], t).series(t, prec) # y_9
-print(h2_inf(t=test_z))
+print(h2_inf(t=test_z_oo))
